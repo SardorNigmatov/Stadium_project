730e649ef7d58a5856aadb2d5fa957e6929ed90f
Shohjaxon <shohjaxon.o.r@gmail.com>
2023-09-14 11:24:23 +0500

    stadiummodels qoshildi

--- stadiums/models.py
+++ stadiums/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
+
+
+
+
+class StadiumsModels(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=300)
+    contact =  models.CharField(
+        max_length=16,
+        blank=True,
+        null=True,
+        validators=[
+          RegexValidator(
+            regex=r'^\+?1?\d{9,15}$',
+            message="Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
+          ),
+        ],
+      )
+    img = models.ImageField(upload_to='news/',blank=True,null=True)
+    stadium_about = models.CharField(max_length=250)
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        db_table = 'stadiums'
 
 # Create your models here.
