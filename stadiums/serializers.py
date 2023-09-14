105ca8c840c63e24f28278861dba82f46fc2a2f4
Shohjaxon <shohjaxon.o.r@gmail.com>
2023-09-14 12:42:38 +0500

    done

--- stadiums/serializers.py
+++ stadiums/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import StadiumsModels
-
+from account.models import CustomUser
 class StadiumsSerializer(serializers.ModelSerializer):
+
 
 
 
--- stadiums/views.py
+++ stadiums/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import StadiumsModels
+from .serializers import StadiumsSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView, CreateAPIView, \
+    RetrieveAPIView, DestroyAPIView, UpdateAPIView
+
+class AllStadiumsView(ListAPIView):
+    queryset = StadiumsModels.objects.all()
+    serializer_class = StadiumsSerializer
+
+class DetailStadiumsView(RetrieveAPIView):
+    queryset = StadiumsModels.objects.all()
+    serializer_class = StadiumsSerializer
+
+class CreateStadiumsView(CreateAPIView):
+    queryset = StadiumsModels.objects.all()
+    serializer_class = StadiumsSerializer
+    permission_classes = (IsAuthenticated,)
+
+class UpdateStadiumsView(UpdateAPIView):
+    queryset = StadiumsModels.objects.all()
+    serializer_class = StadiumsSerializer
+    permission_classes = (IsAuthenticated,)
+
+class DeleteStadiumsView(DestroyAPIView):
+    queryset = StadiumsModels.objects.all()
+    serializer_class = StadiumsSerializer
+    permission_classes = (IsAuthenticated,)
+
 
 # Create your views here.
