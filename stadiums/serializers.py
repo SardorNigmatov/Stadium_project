e4f64c83f1eb35fa5154859017346b5fa9f3dd6a
SardorNigmatov <sardornigmatov2003@gmail.com>
2023-09-14 13:10:24 +0500

    add

--- stadiums/serializers.py
+++ stadiums/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
-from .models import StadiumsModels
+from .models import StadiumsModels, BronModel
 
 class StadiumsSerializer(serializers.ModelSerializer):
-
-
-
     class Meta:
         model = StadiumsModels
         fields = ('name','address','contact','img','stadium_about')
+
+class BronedListSerializer(serializers.ModelSerializer):
+    model = BronModel
+    model = StadiumsModels
+    fileds = ('StadiumsModels.name','first_name','last_name','phone_number','date','begin_time','end_time')
--- stadiums/views.py
+++ stadiums/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from .models import BronModel, StadiumsModels
+from rest_framework.generics import ListAPIView
+from .serializers import BronedListSerializer
 # Create your views here.
+
+class BronedListView(ListAPIView):
+    serializer_class = BronedListSerializer
+    def get(self, request):
+        queryset = BronModel.objects.filter(is_broned=True)
+        if queryset.exists():
+            data = self.serializer_class(queryset, many=True).data
+            return Response(data)
+        else:
+            # Handle the case when there are no broned objects
+            return Response("No broned objects found.")
+
+
+
