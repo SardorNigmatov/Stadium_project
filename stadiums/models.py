fb4d58c2e3a1c5c463a25188378bea8fd512aa53
Mokhirjon <Mokhirjon@test.com>
2023-09-14 12:12:48 +0500

    Mokhirjon

--- stadiums/models.py
+++ stadiums/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import RegexValidator
-
+from datetime import datetime, timedelta
 
 
 
@@ -28,3 +28,22 @@ class StadiumsModels(models.Model):
         db_table = 'stadiums'
 
 # Create your models here.
+class BronModel(models.Model):
+    stadion_id = models.ForeignKey(StadiumsModels, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(
+        max_length=16,
+        blank=True,
+        null=True,
+        validators=[
+          RegexValidator(
+            regex=r'^\+?1?\d{9,15}$',
+            message="Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
+          ),
+        ],
+      )
+    date = models.DateField()
+    begin_time = models.TimeField(default=datetime.now)
+    end_time = models.TimeField(default=datetime.now)
+    is_broned = models.BooleanField(default=False)
